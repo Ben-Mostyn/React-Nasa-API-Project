9d2a6e90fb0034a1d640aeb09e7e6fe3f12a4174
cherrelle <cherrelle_ward@hotmail.co.uk>
2022-09-30 11:39:55 +0100

    css added

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
             element={
               <Card>
                 <PicOfDay />
+                <Weather />
               </Card>
             }
           />
