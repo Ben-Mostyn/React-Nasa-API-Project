e0fe4787639451c7c3a187df3c78f33f03b309c6
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-09-30 09:58:30 +0100

    Browser Update

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,38 +1,38 @@
-// import { useState, useEffect } from 'react';
-// import React from 'react';
-// import '../App.css';
-// import marsImage from '../mars.jpeg';
+// import { useState, useEffect } from "react";
+// import React from "react";
+// import "../App.css";
+// import marsImage from "../mars.jpeg";
 
 // const Weather = (props) => {
-//     const [WeatherData, setWeatherData] = useState({})
+//   const [WeatherData, setWeatherData] = useState({});
 
-//     const fetchData = async () => {
-//         try {
-//             let res = await fetch(
-//                 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json'
-//             )
-//             if (res.status !== 200) {
-//                 throw new Error('oops!!')
-//             }
-//             let data = await res.json();
-//             setWeatherData(data)
-//             console.log(data);
-//         } catch (error) {
-//             console.log('Error', error)
-//         }
+//   const fetchData = async () => {
+//     try {
+//       let res = await fetch(
+//         "http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json"
+//       );
+//       if (res.status !== 200) {
+//         throw new Error("oops!!");
+//       }
+//       let data = await res.json();
+//       setWeatherData(data);
+//       console.log(data);
+//     } catch (error) {
+//       console.log("Error", error);
 //     }
+//   };
 
-//     useEffect(() => {
-//         fetchData();
-//     }, []);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
-//     return (
-//         <div>
-//             <img src={issImage} alt="Logo" style={{}} />
-//             <p>{WeatherData.init}</p>
-//         </div>
-//     )
-// }
+//   return (
+//     <div>
+//       <img src={issImage} alt="Logo" style={{}} />
+//       <p>{WeatherData.init}</p>
+//     </div>
+//   );
+// };
 
 // export default Weather;
 
